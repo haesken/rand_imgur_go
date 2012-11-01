9c674bb5f1b5e07636312dbaefb64b2575979c1b
haesken08 <haesken08@gmail.com>
2012-10-31 23:09:37 -0500

    Add some comments.

--- rand_imgur.go
+++ rand_imgur.go
@@ -10,10 +10,15 @@ import (
 var alphanum = "abcdefghijkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789"
 
 
+// randInt generates a random int between a set minimum and maximum
 func randInt(min int, max int) int {
     return min + rand.Intn(max - min)
 }
 
+// randString generates a random string of a set length.
+// The length argument determines the length of the string to generate,
+// and charset argument serves as the list of possible characters
+// to choose from.
 func randString (length int, charset string) string {
     buffer := make([]byte, length)
 
@@ -24,7 +29,12 @@ func randString (length int, charset string) string {
     return string(buffer)
 }
 
-func genURL() (string, string) {
+// genImgurURL generates a possible url for an image hosted at imgur.com
+// It returns the "name" of the image (five randomized alphanumeric characters)
+// and the respective url for that image.
+// Note: if the image hosted at imgur is not actually a jpg,
+// imgur will respond with the image anyway.
+func genImgurURL() (string, string) {
     base := "http://www.imgur.com/"
     imgurName := randString(5, alphanum)
     imgurURL := base + imgurName + ".jpg"
@@ -34,5 +44,5 @@ func genURL() (string, string) {
 
 func main() {
     rand.Seed(time.Now().UTC().UnixNano())
-    fmt.Println(genURL())
+    fmt.Println(genImgurURL())
 }
