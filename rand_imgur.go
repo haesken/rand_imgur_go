e61e1384f46734e53a2517eee848e666a580110f
haesken08 <haesken08@gmail.com>
2012-10-31 22:52:55 -0500

    Have randString take a charset as a second arg.

--- rand_imgur.go
+++ rand_imgur.go
@@ -6,28 +6,33 @@ import (
     "time"
 )
 
+
+var alphanum = "abcdefghijkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789"
+
+
 func randInt(min int, max int) int {
     return min + rand.Intn(max - min)
 }
 
-func randString (length int) string {
-    var alpha = "abcdefghijkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789"
+func randString (length int, charset string) string {
     buffer := make([]byte, length)
+
     for count := 0 ; count < length ; count++ {
-        buffer[count] = alpha[rand.Intn(len(alpha)-1)]
+        buffer[count] = charset[randInt(0, len(charset))]
     }
+
     return string(buffer)
 }
 
 func genURL() (string, string) {
-    var base = "http://www.imgur.com/"
-    imgurName := randString(5)
+    base := "http://www.imgur.com/"
+    imgurName := randString(5, alphanum)
     imgurURL := base + imgurName + ".jpg"
+
     return imgurName, imgurURL
 }
 
 func main() {
     rand.Seed(time.Now().UTC().UnixNano())
-    fmt.Println(randString(5))
     fmt.Println(genURL())
 }
