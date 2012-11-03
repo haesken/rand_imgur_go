5b644962377fc3dcb360a706f57e44b256a13396
haesken08 <haesken08@gmail.com>
2012-11-03 13:17:18 -0500

    Add more comments.

--- rand_imgur.go
+++ rand_imgur.go
@@ -17,16 +17,12 @@ import (
 
 var alphanum = "abcdefghijkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789"
 
-
 // randInt generates a random int between a set minimum and maximum
 func randInt(min int, max int) int {
     return min + rand.Intn(max - min)
 }
 
 // randString generates a random string of a set length.
-// The length argument determines the length of the string to generate,
-// and charset argument serves as the list of possible characters
-// to choose from.
 func randString (length int, charset string) string {
     buffer := make([]byte, length)
 
@@ -40,11 +36,11 @@ func randString (length int, charset string) string {
 // genImgurURL generates a possible url for an image hosted at imgur.com
 // It returns the "name" of the image (five randomized alphanumeric characters)
 // and the respective url for that image.
-// Note: if the image hosted at imgur is not actually a jpg,
-// imgur will respond with the image anyway.
 func genImgurURL() (string, string) {
     base := "http://www.imgur.com/"
     imgurName := randString(5, alphanum)
+    // Note: if the image hosted at imgur is not actually a jpg,
+    // imgur will respond with the image anyway.
     imgurURL := base + imgurName + ".jpg"
 
     return imgurName, imgurURL
@@ -55,9 +51,9 @@ func genImgurURL() (string, string) {
 func hashImage(image []byte) string {
     hasher := md5.New()
     hasher.Write(image)
+
     return hex.EncodeToString(hasher.Sum(nil))
 }
-
 
 // getUrl fetches a url with a http GET.
 // It returns if the contents of the the response.
@@ -73,9 +69,12 @@ func getUrl(url string) []byte {
     }
 
     resp.Body.Close()
+
     return contents
 }
 
+// pathExists determines if a file/directory already exists.
+// It returns a bool, and an error if it gets one.
 func pathExists(path string) (bool, error) {
     _, err := os.Stat(path)
     if err == nil {
@@ -87,6 +86,8 @@ func pathExists(path string) (bool, error) {
     return false, err
 }
 
+// writeFile writes the contents of a byte slice to the specified
+// directory.
 func writeFile(contents []byte, pathDirectory string, filename string) int {
     pathDirectoryExists, err := pathExists(pathDirectory)
     if err != nil {
@@ -101,6 +102,9 @@ func writeFile(contents []byte, pathDirectory string, filename string) int {
     return 0
 }
 
+
+// findImages searches imgur for images. It requests a random image,
+// but only writes it to disk if it is not the 404 gif.
 func findImages() {
     for {
         imgurName, imgurURL := genImgurURL()
@@ -108,6 +112,7 @@ func findImages() {
         image_hash := hashImage(image)
         timestamp := strconv.FormatInt(time.Now().Unix(), 10)
 
+        // Hash here is the 404 gif's hash.
         if image_hash != "d835884373f4d6c8f24742ceabe74946" {
             fmt.Println("Found image!")
             fmt.Println(imgurURL)
@@ -116,6 +121,7 @@ func findImages() {
             writeFile(image, pathDirectory, filename)
         }
 
+        // Throttle connects to one per second per thread.
         time.Sleep(1 * time.Second)
     }
 }
